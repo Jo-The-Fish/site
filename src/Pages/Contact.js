34a00334a48992d7fdaaaaa0936569a750345e32
Sharlene May <sharleneminosa@gmail.com>
2020-07-27 17:23:51 -0700

    contact page frontend layout

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,10 +1,24 @@
 import React from "react";
+import { Jumbotron, Container, Form, Button } from "react-bootstrap";
 import "./styles.css";
 
-export default function Contact () {
-    return (
-        <div className="jothefish">
-            
-        </div>
-    );
+export default function Contact() {
+  return (
+    <div>
+      <Jumbotron>
+        <h2>Get In Touch</h2>
+      </Jumbotron>
+      <Container className="jothefish">
+        <p>Want to find out more? Contact us!</p>
+        <Form>
+          <Form.Control className="contactinput" type="text" placeholder="Name" />
+          <Form.Control className="contactinput" type="email" placeholder="Email" />
+          <Form.Control className="contactinput" as="textarea" placeholder="Message" rows="5" />
+          <Button variant="primary" type="submit">
+            Send Message
+          </Button>
+        </Form>
+      </Container>
+    </div>
+  );
 }
