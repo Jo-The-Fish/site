827b9b1bf9e5c4c15e7020c6f5fbdb7b77878a25
Sharlene May <sharleneminosa@gmail.com>
2020-08-05 10:13:09 -0700

    set up server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const Mailchimp = require('mailchimp-api-v3');
+require('dotenv').config();
+
+var mc_api_key = process.env.MAILCHIMP_API_KEY;
+var list_id = process.env.MAILING_LIST_ID;
+
+const app = express();
+const mailchimp = new Mailchimp(mc_api_key);
+
+const port = process.env.PORT || 3001;
+app.listen(port);
+
+//routes
+app.get('/api/memberList', (req, res) => {
+    mailchimp.get(`/lists/${list_id}/members`)
+    .then(function(results){
+      res.send(results);
+    })
+    .catch(function(err){
+      res.send(err);
+    });
+  });
+
+console.log(`express app listening on port ${port}`);
--- src/Components/WantEarlyAccess/wantEarlyAccess.js
+++ src/Components/WantEarlyAccess/wantEarlyAccess.js
@@ -1,23 +1,30 @@
 import React, { useState } from "react";
 import { Container, Form, Button } from "react-bootstrap";
-import jsonp from 'jsonp';
-import queryString from 'query-string';
+import MailchimpSubscribe from "react-mailchimp-subscribe"
+// import jsonp from 'jsonp';
+// import queryString from 'query-string';
 
 export default function SignUpEarlyAccess() {
 
   const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
 
   const subscribe = () => {
-    const formData = {
-      EMAIL: email
-    };
-    jsonp(`${process.env.REACT_APP_MAILCHIMP_URL}${queryString.stringify(formData)}`, { param: 'c' }, (err, data) => {
-      console.log('err:', err);
-      console.log('data:', data);
-    });
+
+  //   const formData = {
+  //     EMAIL: email
+  //   };
+  //   jsonp(`${process.env.REACT_APP_MAILCHIMP_URL}${queryString.stringify(formData)}`, { param: 'c' }, (err, data) => {
+  //     console.log('err:', err);
+  //     console.log('data:', data);
+  //   });
   }
+
+  // const url = process.env.MAILCHIMP_URL
+
   return (
     <div>
+      {/* <MailchimpSubscribe url={url} /> */}
       <Container className="form" id="earlyAccess">
         <h2 className="whiteText">Want early access?</h2>
         <Form>
@@ -25,6 +32,7 @@ export default function SignUpEarlyAccess() {
           <Button variant="primary" type="submit" onClick={subscribe}>
             Submit
           </Button>
+          {/* {error ? error : ""} */}
         </Form>
       </Container>
     </div>
