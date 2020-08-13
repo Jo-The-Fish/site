9b803bccdb8e325633ef6babd2aed54ca69aeff5
Sharlene May <sharleneminosa@gmail.com>
2020-08-12 21:04:56 -0700

    Updates

---
+++ precache-manifest.dfa02e80ac1dd962c17bbea1a5e4968a.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6064ae8bcfd002c4cf0ccd14ce2df289",
+    "url": "/site/index.html"
+  },
+  {
+    "revision": "777e9899c989201abeea",
+    "url": "/site/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2ed162e705db5dc31bc1",
+    "url": "/site/static/css/main.114c4e87.chunk.css"
+  },
+  {
+    "revision": "777e9899c989201abeea",
+    "url": "/site/static/js/2.c6a39dca.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/site/static/js/2.c6a39dca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ed162e705db5dc31bc1",
+    "url": "/site/static/js/main.1497a2a4.chunk.js"
+  },
+  {
+    "revision": "5c700c4167de7c0f7930",
+    "url": "/site/static/js/runtime-main.0b5d2485.js"
+  },
+  {
+    "revision": "bc7f89bc1807050874857c240517e953",
+    "url": "/site/static/media/JennyLee.bc7f89bc.png"
+  },
+  {
+    "revision": "61b4175b923898df83f2e32be1e3ac2f",
+    "url": "/site/static/media/Jo-confused-10-flatB.61b4175b.svg"
+  },
+  {
+    "revision": "c94f064b62f494b88256a045b863e4ab",
+    "url": "/site/static/media/Jo-dead-10-flat-B.c94f064b.svg"
+  },
+  {
+    "revision": "78fd574eea05bffd7d6f33609174e3e9",
+    "url": "/site/static/media/Jo-happy-10-flat-green-fin.78fd574e.svg"
+  },
+  {
+    "revision": "bbc4e23dccbe503562c80075505a7e05",
+    "url": "/site/static/media/Jo-happy-blue-10-flat.bbc4e23d.svg"
+  },
+  {
+    "revision": "8a9cb6ab749ec172856f3c90c431427a",
+    "url": "/site/static/media/Jo-neutral-10-flat.8a9cb6ab.svg"
+  },
+  {
+    "revision": "ce460a2267f0e75022e85ddcabc7523c",
+    "url": "/site/static/media/allan.ce460a22.jpg"
+  },
+  {
+    "revision": "576a0ed18416cf55af755ee056cffa55",
+    "url": "/site/static/media/down_arrow.576a0ed1.svg"
+  },
+  {
+    "revision": "049bc5ced22929035a41b6f5e52c25d0",
+    "url": "/site/static/media/extension-img.049bc5ce.svg"
+  },
+  {
+    "revision": "8232463c587459f3c3f90c03bec7447b",
+    "url": "/site/static/media/facebook.8232463c.svg"
+  },
+  {
+    "revision": "3a6d1853a62ddf860b8fc2a1ee853355",
+    "url": "/site/static/media/instagram.3a6d1853.svg"
+  },
+  {
+    "revision": "34b99770f4522123f017d3df42d3b2bf",
+    "url": "/site/static/media/jeffrey.34b99770.jpeg"
+  },
+  {
+    "revision": "e3e91d05b613b5c25745874df5f60fe9",
+    "url": "/site/static/media/jinny.e3e91d05.JPG"
+  },
+  {
+    "revision": "00813be3fa6a5e8e2432edd19d8fc8e7",
+    "url": "/site/static/media/maggie.00813be3.jpg"
+  },
+  {
+    "revision": "b0303e61ee5c1c6015f51a1305ac466b",
+    "url": "/site/static/media/micha.b0303e61.jpg"
+  },
+  {
+    "revision": "3fefee1af29d5434362164803447cd65",
+    "url": "/site/static/media/mozilla.3fefee1a.svg"
+  },
+  {
+    "revision": "472c74d07f6bbd394d3aab6b1669b54d",
+    "url": "/site/static/media/partial-jo.472c74d0.svg"
+  },
+  {
+    "revision": "cf2afd637b6a4a602d1157889e0767aa",
+    "url": "/site/static/media/shaded-Jo-confused.cf2afd63.svg"
+  },
+  {
+    "revision": "cac7ca6a04c6e8afdcd17614013363f5",
+    "url": "/site/static/media/shaded-Jo-happy.cac7ca6a.svg"
+  },
+  {
+    "revision": "6c1bd6958d96a3cc8f6d8b6dd56f4965",
+    "url": "/site/static/media/shaded-Jo-neutral.6c1bd695.svg"
+  },
+  {
+    "revision": "7be95df5c029ed97e77dabb96e6c2bd1",
+    "url": "/site/static/media/shaded-Jo-sick.7be95df5.svg"
+  },
+  {
+    "revision": "699eb84581f0c7e1df8319896ede2efb",
+    "url": "/site/static/media/sharlene.699eb845.png"
+  },
+  {
+    "revision": "93e0760b8f6ae8010351cd14a6286b3a",
+    "url": "/site/static/media/simon.93e0760b.jpg"
+  },
+  {
+    "revision": "485ad9c0ee1f87f242e869b8fc7e939d",
+    "url": "/site/static/media/suubi.485ad9c0.jpg"
+  },
+  {
+    "revision": "5be5add7c69996d9a9fff0fc693adf8d",
+    "url": "/site/static/media/twitter.5be5add7.svg"
+  },
+  {
+    "revision": "acbe882c0ece6291f082c789ceba1506",
+    "url": "/site/static/media/wave_background.acbe882c.svg"
+  }
+]);
